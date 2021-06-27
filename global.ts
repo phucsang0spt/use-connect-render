849497c60c09596c7437de0a6963696bf5da8798
phucsang <phucsang0spt@gmai.com>
2021-06-27 07:03:51 +0700

    Support react native

--- global.ts
+++ global.ts
@@ -1,28 +1,27 @@
 import { useMemo } from "react";
 
-const _global = {};
+//@ts-ignore
+const _global = typeof global === "undefined" ? {} : global;
 
 export function useGlobal<T extends Record<string, any>>(
   initialValue?: T,
   onInitial?: (initialValue?: T) => void
 ) {
   return useMemo(() => {
-    const global = (typeof window === "undefined"
-      ? _global
-      : window) as any as Record<string, any>;
+    const __global = getGlobal();
     if (initialValue) {
       for (const k of Object.keys(initialValue)) {
-        if (!global.hasOwnProperty(k)) {
-          global[k] = initialValue![k];
+        if (!__global.hasOwnProperty(k)) {
+          __global[k] = initialValue![k];
         }
       }
     }
-    onInitial && onInitial(global as any);
-    return global as T;
+    onInitial && onInitial(__global as any);
+    return __global as T;
   }, []);
 }
 
 export function getGlobal<T extends Record<string, any>>() {
-  const global = (typeof window === "undefined" ? _global : window) as any;
-  return global as T;
+  const __global = (typeof window === "undefined" ? _global : window) as any as Record<string, any>;
+  return __global as T;
 }
